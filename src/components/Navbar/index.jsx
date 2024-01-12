72ef9a6baab3f1d6aa786a14029585a9e70e0c07
Farouk <faroukwebdesign@gmail.com>
2024-01-12 04:52:51 +0100

    style(navbar): fix navbar logo issue and implement initial navbar responsiveness
    fixed the logo where it was bigger on large screens compared to laptop , implemented the first
    initial responsiveness for the navbar to match all device screens
    
    fix #1

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,52 +1,39 @@
-import { Navbar, IconButton, iconButton, Input } from "@material-tailwind/react"
 import { IoSearchCircleOutline } from "react-icons/io5"
 import Image from "next/image"
 import Link from "next/link"
+import { Button, Input } from "@material-tailwind/react"
 
 export default function Nav() {
   return (
     <section className="w-full">
-      <Navbar
-        color="blue-gray"
-        className=" bg-dark-100 py-3 min-w-full px-20 rounded-none"
-      >
-        <div className="flex items-center justify-between w-full text-white">
-          <div className="flex gap-5 justify-start w-full items-center">
-            <figure className="h-full w-[5%]">
-              <Image
-                src="/images/logo.png"
-                alt="logo"
-                height={500}
-                width={500}
-                priority
-                className="w-full h-full object-cover"
-              />
-            </figure>
-            <div className="flex justify-evenly items-center gap-4 h-full">
-              <Link href="/">Browse</Link>
-              <Link href="/movies">Movies</Link>
-              <Link href="/series">Tv Shows</Link>
-            </div>
-          </div>
-
-          <div className="relative flex w-full gap-2 md:w-max bg-dark-50">
-            <Input
-              type="search"
-              color="white"
-              label="Type here..."
-              containerProps={{
-                className: "min-w-[288px]",
-              }}
-            />
-            <IconButton
-              size="sm"
-              className="!absolute right-1 top-1 rounded bg-inherit"
-            >
-              <IoSearchCircleOutline className="text-white w-6 h-6" />
-            </IconButton>
+      <nav className="bg-dark-200 w-full h-16 flex justify-between items-center px-5 lg:px-20">
+        <div className="flex gap-10 items-center h-full w-full">
+          <figure className="w-12">
+            <Image src="/images/logo.png" alt="logo" height={200} width={200} />
+          </figure>
+          <div className="gap-8 hidden lg:flex lg:justify-between uppercase lg:items-center h-full">
+            <Link href="/">Browse</Link>
+            <Link href="/">Movies</Link>
+            <Link href="/">Tv Shows</Link>
           </div>
         </div>
-      </Navbar>
+        <div className="relative hidden items-center w-full gap-2 md:w-max lg:block ">
+          <Input
+            type="search"
+            color="white"
+            label="Search ..."
+            containerProps={{
+              className: "min-w-[288px] bg-dark-50",
+            }}
+          />
+          <Button
+            size="sm"
+            className="!absolute right-1 top-[2px] bg-transparent shadow-none text-gray-400"
+          >
+            <IoSearchCircleOutline className="w-5 h-5" />
+          </Button>
+        </div>
+      </nav>
     </section>
   )
 }
