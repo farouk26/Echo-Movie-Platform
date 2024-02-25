dc57239af86550addb27c3300cab54a48266a0ce
Farouk <faroukwebdesign@gmail.com>
2024-02-25 15:07:42 +0100

    style(navbar , footer , daisyui update): update navbar and footer basic initial ui , update daisyui
    updated the navbar to give it an initial ui , updated footer and updated daisyui to its latest
    update
    
    fix #1

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,41 +4,47 @@ import Link from "next/link"
 
 export default function Nav() {
   return (
-    <div className="navbar bg-dark-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+    <div className="navbar bg-dark-100 px-20">
+      <div className="navbar-start">
+        <figure className="min-w-fit h-8 my-3">
+          <Image
+            src="/images/logo.png"
+            alt="logo"
+            height={500}
+            width={500}
+            className="w-full h-full object-cover"
+          />
+        </figure>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+      <div className="navbar-center w-1/3 flex justify-center items-center">
+        <ul className="flex gap-2 w-full justify-evenly items-center">
           <li>
-            <a>Link</a>
+            <Link href="/">Browse</Link>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-dark-50 rounded-t-none">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <Link href="/">Movies</Link>
+          </li>
+          <li>
+            <Link href="/">Tv Shows</Link>
           </li>
         </ul>
+      </div>
+      <div className="navbar-end">
+        <label className="input input-bordered flex items-center gap-2">
+          <input type="text" className="grow" placeholder="Search" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
       </div>
     </div>
   )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,14 @@
+import Footer from "@/components/Footer"
 import Nav from "@/components/Navbar"
 
 export default function Home() {
   return (
-    <main className="w-full">
+    <main className="w-full min-h-screen">
       <Nav />
+      <div className="bg-white min-h-fit w-full text-black text-4xl text-center p-4">
+        Page Test Text
+      </div>
+      <Footer />
     </main>
   )
 }
